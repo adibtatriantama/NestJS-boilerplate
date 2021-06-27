f32f4e7fa29eb259f5e416167a52e84c3850eacb
Adibta Triantama <adibtatriantama@gmail.com>
2021-06-27 19:30:29 +0700

    feat: register fastify-helmet

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,15 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { fastifyHelmet } from 'fastify-helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+
+  await app.register(fastifyHelmet);
 
   await app.listen(3000, '0.0.0.0');
 }
